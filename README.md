594df1d9444d6cd40ffffd2a3364c657f22588c3
paulbrickwell <paulbrickwell@gmail.com>
2021-08-05 22:43:06 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nova9 Design Template
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Home</p>
+    <p class="accent">With SCSS!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  metaInfo: {
+    title: "Nova9",
+    titleTemplate: null
+  },
+};
+</script>
+
+<style lang="scss">
+.accent {
+  background-color: $secondary-color;
+}
+</style>
